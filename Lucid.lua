4a4ca99eb0403d4c401413d02083a094b68c8a35
Zunes-Bypassed <dnqvinh25012010@gmail.com>
2025-09-28 18:39:55 +0700

    Add files via upload

--- Lucid.lua
+++ Lucid.lua
@@ -1956,13 +1956,12 @@ Components.TitleBar = (function()
 			local Button = { Callback = Callback or function() end }
 
 			Button.Frame = New("ImageButton", {
-				Size = UDim2.new(0, 36, 1, -6),
-				AnchorPoint = Vector2.new(1, 0),
+				Size = UDim2.new(0, 28, 0, 28),
 				BackgroundTransparency = 1,
 				Parent = Parent,
 				Position = Pos,
 				Image = Icon,
-			}, { New("UICorner", { CornerRadius = UDim.new(0, 8) }) })
+			}, { New("UICorner", { CornerRadius = UDim.new(0, 6) }) })
 
 			local _, SetTransparency = Creator.SpringMotor(1, Button.Frame, "BackgroundTransparency")
 
@@ -2002,20 +2001,20 @@ Components.TitleBar = (function()
 				}, { New("UICorner", { CornerRadius = UDim.new(1, 0) }) }),
 				New("Frame", {
 					BackgroundTransparency = 1,
-					Size = UDim2.new(0, 320, 0, 28),
+					Size = UDim2.new(0, 280, 0, 28),
 				}, {
 					New("UIListLayout", {
 						FillDirection = Enum.FillDirection.Horizontal,
 						SortOrder = Enum.SortOrder.LayoutOrder,
 						VerticalAlignment = Enum.VerticalAlignment.Center,
-						Padding = UDim.new(0, 8),
+						Padding = UDim.new(0, 6),
 					}),
 					New("TextLabel", {
 						Text = Config.Title,
 						RichText = true,
 						FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json",
 							Enum.FontWeight.SemiBold, Enum.FontStyle.Normal),
-						TextSize = 15,
+						TextSize = 14,
 						BackgroundTransparency = 1,
 						TextColor3 = Color3.fromRGB(255, 255, 255),
 						TextXAlignment = Enum.TextXAlignment.Left,
@@ -2028,7 +2027,7 @@ Components.TitleBar = (function()
 						RichText = true,
 						FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json",
 							Enum.FontWeight.Regular, Enum.FontStyle.Italic),
-						TextSize = 12,
+						TextSize = 11,
 						BackgroundTransparency = 1,
 						TextColor3 = Color3.fromRGB(220, 220, 220),
 						TextXAlignment = Enum.TextXAlignment.Left,
@@ -2045,7 +2044,7 @@ Components.TitleBar = (function()
 			}),
 		})
 
-		TitleBar.CloseButton = BarButton(Components.Assets.Close, UDim2.new(1, -4, 0, 6), TitleBar.Frame, function()
+		TitleBar.CloseButton = BarButton(Components.Assets.Close, UDim2.new(1, -4, 0, 10), TitleBar.Frame, function()
 			Library.Window:Dialog({
 				Title = "Lucid",
 				Content = "Are you sure you want to close?",
@@ -2056,15 +2055,15 @@ Components.TitleBar = (function()
 			})
 		end)
 
-		TitleBar.MaxButton = BarButton(Components.Assets.Max, UDim2.new(1, -44, 0, 6), TitleBar.Frame, function()
+		TitleBar.MaxButton = BarButton(Components.Assets.Max, UDim2.new(1, -38, 0, 10), TitleBar.Frame, function()
 			Config.Window.Maximize(not Config.Window.Maximized)
 		end)
 
-		TitleBar.MinButton = BarButton(Components.Assets.Min, UDim2.new(1, -84, 0, 6), TitleBar.Frame, function()
+		TitleBar.MinButton = BarButton(Components.Assets.Min, UDim2.new(1, -72, 0, 10), TitleBar.Frame, function()
 			Library.Window:Minimize()
 		end)
 
-		TitleBar.SearchButton = BarButton(Components.Assets.Search, UDim2.new(1, -124, 0, 6), TitleBar.Frame, function()
+		TitleBar.SearchButton = BarButton(Components.Assets.Search, UDim2.new(1, -106, 0, 10), TitleBar.Frame, function()
 			print("Search button clicked")
 		end)
 

535c7cbe59210030205da2c52bca2ef14ab8f835
Zunes-Bypassed <dnqvinh25012010@gmail.com>
2025-10-05 11:17:40 +0700

    Add files via upload

--- Lucid.lua
+++ Lucid.lua
@@ -2183,12 +2183,14 @@ Components.Window = (function()
 		Window.AcrylicPaint = Acrylic.AcrylicPaint()
 
 		local Selector = New("Frame", {
-			Size = UDim2.fromOffset(4, 0),
-			BackgroundColor3 = Color3.fromRGB(76, 194, 255),
-			Position = UDim2.fromOffset(0, 17),
-			AnchorPoint = Vector2.new(0, 0.5),
-			ThemeTag = { BackgroundColor3 = "Accent" },
-		}, { New("UICorner", { CornerRadius = UDim.new(0, 3) }) })
+        	Size = UDim2.fromOffset(4, 16),
+        	BackgroundColor3 = Color3.fromRGB(76, 194, 255),
+        	Position = UDim2.new(0, 0, 0.5, 0),
+        	AnchorPoint = Vector2.new(0, 0.5),
+        	ThemeTag = { BackgroundColor3 = "Accent" },
+        }, {
+        	New("UICorner", { CornerRadius = UDim.new(0, 3) })
+        })
 
 		local ResizeStartFrame = New("Frame", {
 			Size = UDim2.fromOffset(18, 18),
@@ -2955,9 +2957,9 @@ function Element:New(Idx, Config)
             })
     
             local ButtonSelector = New("Frame", {
-            	Size = UDim2.new(0, 4, 0, 26),
+            	Size = UDim2.fromOffset(4, 20),
             	BackgroundColor3 = Color3.fromRGB(76, 194, 255),
-            	Position = UDim2.new(0, -2, 0.5, 1),
+            	Position = UDim2.new(0, -2, 0.5, 1), 
             	AnchorPoint = Vector2.new(0, 0.5),
             	Visible = false,
             	ThemeTag = { BackgroundColor3 = "Accent" }
